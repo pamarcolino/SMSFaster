be30cfe368e952f82d80095e5dfc7ceab6456ad6
PAULINHO-PC\paulo <pamarcolino@gmail.com>
2018-11-09 23:43:35 -0200

    Initial commit

---
+++ app/src/main/java/br/com/softpam/smsfaster/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.softpam.smsfaster
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
